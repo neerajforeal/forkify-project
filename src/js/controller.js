1a4d95ac3463f6353969265eb7af8f895fe92e20
neeraj <neerajpallikonda34@gmail.com>
2025-11-12 16:40:45 +0530

    Implement search and results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,15 +1,17 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
+import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-const recipeContainer = document.querySelector(".recipe");
+if (module.hot) {
+  module.hot.accept();
+}
 
 // NEW API URL (instead of the one shown in the video)
 // https://forkify-api.jonas.io
-
-///////////////////////////////////////
 
 const controlRecipes = async function () {
   try {
@@ -19,17 +21,31 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
-    // 1. Loading recipe
     await model.loadRecipe(id);
 
-    // 2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    await model.loadSearchResults(query);
+
+    resultsView.render(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
