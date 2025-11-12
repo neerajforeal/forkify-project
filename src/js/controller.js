b36a83d60257ccd922fe2b088a04b8ccf08f35f2
neeraj <neerajpallikonda34@gmail.com>
2025-11-12 12:25:20 +0530

    Implement event handler and error, success msgs

--- src/js/controller.js
+++ src/js/controller.js
@@ -25,12 +25,11 @@ const controlRecipes = async function () {
     // 2. Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-["hashchange", "load"].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes),
-);
-// window.addEventListener("hashchange", controlRecipes);
-// window.addEventListener("load", controlRecipes);
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
